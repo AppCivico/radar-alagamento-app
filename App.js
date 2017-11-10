02c7d615f91783b87fd9f51ee1d5589c1e7da81e
Laís Tomaz <lais@eokoe.com>
2017-11-10 19:13:28 -0200

    Add Navigation to App root

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator, NavigationActions } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import Welcome from './src/scripts/containers/Welcome';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = StackNavigator({
+    Welcome: {screen: Welcome},
+},
+{
+    initialRouteName: 'Welcome',
+    headerMode: 'none'
 });
+
+export default App;
