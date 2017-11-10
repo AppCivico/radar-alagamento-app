81109a41871c533c881427478e2e7ca8e42935cc
Laís Tomaz <lais@eokoe.com>
2017-11-10 19:14:22 -0200

    Add common style file

---
+++ src/styles/common.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+	StyleSheet
+} from 'react-native';
+
+const common = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 15,
+	},
+});
+
+export default common;
