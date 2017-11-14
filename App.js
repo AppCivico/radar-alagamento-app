3c917842e1df49019f29c3f11acd701b5809a468
Laís Tomaz <lais@eokoe.com>
2017-11-14 19:27:00 -0200

    Add tutorial as initial Route

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StackNavigator } from 'react-navigation';
 
 import Welcome from './src/containers/Welcome';
+import Tutorial from './src/containers/Tutorial';
 
 const App = StackNavigator(
 	{
 		Welcome: { screen: Welcome },
+		Tutorial: { screen: Tutorial },
 	},
 	{
-		initialRouteName: 'Welcome',
+		initialRouteName: 'Tutorial',
 		headerMode: 'none',
 	},
 );
