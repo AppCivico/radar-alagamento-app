7a2926035b8e137b5edac1fed6ebfcc1a9a00b52
Laís Tomaz <lais@eokoe.com>
2017-11-17 16:55:58 -0200

    Enhance tutorial style

--- src/styles/containers/tutorial.js
+++ src/styles/containers/tutorial.js
@@ -3,12 +3,25 @@ import { StyleSheet } from 'react-native';
 const tutorial = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: '#fff',
 	},
 	view: {
 		flex: 1,
+		paddingTop: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 	image: {
+		flex: 1,
 		resizeMode: 'contain',
+		width: '80%',
+		height: 'auto',
+	},
+	text: {
+		flex: 1,
+		textAlign: 'center',
+		paddingTop: 20,
+		color: '#5d5d5d',
 	},
 });
 
