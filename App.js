d8690fafd166e077355fe8f4779c1f722d06dd54
Laís Tomaz <lais@eokoe.com>
2017-11-17 19:35:16 -0200

    Import font before mounting app

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import { StackNavigator } from 'react-navigation';
+import { AppLoading, Font } from 'expo';
+
+import Roboto from './src/assets/fonts/Raleway-Regular.ttf';
+import RobotoMedium from './src/assets/fonts/Raleway-Medium.ttf';
+import RobotoBold from './src/assets/fonts/Raleway-Bold.ttf';
 
 import Welcome from './src/containers/Welcome';
 import Tutorial from './src/containers/Tutorial';
 
-const App = StackNavigator(
+const FirstLaunchNavigation = StackNavigator(
 	{
 		Welcome: { screen: Welcome },
 		Tutorial: { screen: Tutorial },
@@ -13,5 +19,35 @@ const App = StackNavigator(
 		headerMode: 'none',
 	},
 );
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loaded: false,
+		};
+	}
+
+	componentWillMount() {
+		this.loadAssetsAsync();
+	}
+
+	loadAssetsAsync = async () => {
+		await Font.loadAsync({
+			roboto: Roboto,
+			robotoMedium: RobotoMedium,
+			robotoBold: RobotoBold,
+		});
+		this.setState({ loaded: true });
+	};
+
+	render() {
+		if (!this.state.loaded) {
+			return <AppLoading />;
+		}
+
+		return <FirstLaunchNavigation />;
+	}
+}
 
 export default App;
