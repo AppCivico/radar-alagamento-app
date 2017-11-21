63b14899f1a402fce52c3be9c7c62cd3b3ddb8d8
Laís Tomaz <lais@eokoe.com>
2017-11-21 17:51:24 -0200

    Change route if already launched

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types, class-methods-use-this */
 import React from 'react';
-import { View, Image } from 'react-native';
+import { AsyncStorage, View, Image } from 'react-native';
 
 import welcome from '../styles/containers/welcome';
 
@@ -9,10 +9,35 @@ import background from '../assets/images/elements_bg.png';
 
 
 class Welcome extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			firstRoute: 'Tutorial',
+		};
+
+		this.changeRoute = this.changeRoute.bind(this);
+	}
+
 	componentDidMount() {
+		try {
+			const value = AsyncStorage.getItem('alreadyLaunched');
+			if (value == null) {
+				AsyncStorage.setItem('alreadyLaunched', true);
+				this.changeRoute('Tutorial');
+			}	else {
+				this.changeRoute('Districts');
+			}
+		} catch (error) {
+			// Error retrieving data
+		}
+
 		setTimeout(() => {
-			this.props.navigation.navigate('Tutorial');
+			this.props.navigation.navigate(this.state.firstRoute);
 		}, 3000);
+	}
+
+	changeRoute(route) {
+		this.setState({ firstRoute: route });
 	}
 
 	render() {
