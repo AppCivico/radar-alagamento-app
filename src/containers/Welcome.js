fb59f10e63fed9b15a0d094eb4597f7d6f05d021
Laís Tomaz <lais@eokoe.com>
2017-11-21 17:04:44 -0200

    Add timeout navigation to Welcome page

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types, class-methods-use-this */
 import React from 'react';
 import { View, Image } from 'react-native';
 
@@ -6,17 +7,28 @@ import welcome from '../styles/containers/welcome';
 import logo from '../assets/images/logo_radar.png';
 import background from '../assets/images/elements_bg.png';
 
-const Welcome = () => (
-	<View style={welcome.container}>
-		<Image
-			source={logo}
-			style={welcome.logo}
-		/>
-		<Image
-			source={background}
-			style={welcome.background}
-		/>
-	</View>
-);
+
+class Welcome extends React.Component {
+	componentDidMount() {
+		setTimeout(() => {
+			this.props.navigation.navigate('Tutorial');
+		}, 3000);
+	}
+
+	render() {
+		return (
+			<View style={welcome.container}>
+				<Image
+					source={logo}
+					style={welcome.logo}
+				/>
+				<Image
+					source={background}
+					style={welcome.background}
+				/>
+			</View>
+		);
+	}
+}
 
 export default Welcome;
