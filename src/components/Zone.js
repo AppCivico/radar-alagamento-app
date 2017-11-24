d9b635362f9e2fb1af13919418cd6604eb1074c9
Laís Tomaz <lais@eokoe.com>
2017-11-24 18:36:39 -0200

    Trying to make select district work

--- src/components/Zone.js
+++ src/components/Zone.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, CheckBox } from 'react-native';
 
 import { colors } from '../styles/variables';
 
@@ -15,11 +15,69 @@ const style = StyleSheet.create({
 		borderColor: 'black',
 		borderWidth: 2,
 	},
+	districts: {
+		flex: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+	district: {
+		minWidth: '50%',
+		flexDirection: 'row',
+	},
 });
 
 class Zone extends React.Component {
-	renderDistrict(item) {
-		return <Text key={`district-${item.id}`}>{item.name}</Text>;
+	constructor() {
+		super();
+
+		this.state = {
+			selectedDistricts: [],
+			districts: [],
+		};
+
+		this.addDistrict = this.addDistrict.bind(this);
+		this.changeDistricts = this.changeDistricts.bind(this);
+	}
+
+	componentWillMount() {
+		this.changeDistricts();
+	}
+
+	changeDistricts() {
+		const districts = [...this.props.districts];
+		districts.map(item => (item.checked = false));
+		this.setState({ districts });
+	}
+
+	addDistrict(item) {
+		const selectedDistricts = [...this.state.selectedDistricts];
+		selectedDistricts.push(item);
+		this.setState({ selectedDistricts });
+	}
+
+	selectDistrict(item, i) {
+		console.log(item.value);
+		console.log(this.state.districts[i].checked);
+		const district = this.state.districts[i];
+		const districts = [...this.state.districts];
+		district.checked = !this.state.districts[i].checked;
+		console.log(district);
+		districts[i] = district;
+		this.setState({ districts });
+	}
+
+	renderDistrict(item, i) {
+		return (
+			<View style={style.district} key={item.id}>
+				<CheckBox
+					title={item.name}
+					onValueChange={() => this.selectDistrict(item, i)}
+					checked={this.state.districts[i].checked}
+					value={this.state.districts[i].checked}
+				/>
+				<Text>{item.name}</Text>
+			</View>
+		);
 	}
 
 	render() {
@@ -27,7 +85,10 @@ class Zone extends React.Component {
 			<View style={style.container}>
 				<View style={style.zone}>
 					<Text>Zona {this.props.name}</Text>
-					{this.props.districts.map(item => this.renderDistrict(item))}
+					<Text>{this.props.districts.length} distritos</Text>
+					<View style={style.districts}>
+						{this.state.districts.map((item, i) => this.renderDistrict(item, i))}
+					</View>
 				</View>
 			</View>
 		);
