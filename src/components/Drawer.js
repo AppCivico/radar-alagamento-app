9664206b1fc5047a8377cb615a27eea2125b4d22
Laís Tomaz <lais@eokoe.com>
2017-11-30 19:34:59 -0200

    Change Alert route in Drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -73,7 +73,7 @@ class Drawer extends React.Component {
 				{
 					title: 'Alertas',
 					icon: alerts,
-					path: 'alerts',
+					path: 'Notifications',
 				},
 				{
 					title: 'Meus distritos',
